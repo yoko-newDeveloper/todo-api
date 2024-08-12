25942af5495ff1f0ed707823ff475f0eb8f7d264
yochan0120 <yoko_whiterosa.alba@gamma.ocn.ne.jp>
2024-08-12 19:01:22 +0900

    healthチェックの実装

--- src/main/java/com/example/todo_api/Controller/HealthController.java
+++ src/main/java/com/example/todo_api/Controller/HealthController.java
@@ -1,6 +1,14 @@
 package com.example.todo_api.Controller;
 
 import com.example.todoapi.controller.HealthApi;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HealthController implements HealthApi {
+
+    @Override
+    public ResponseEntity<Void> healthGet() {
+        return ResponseEntity.ok().build();
+    }
 }
