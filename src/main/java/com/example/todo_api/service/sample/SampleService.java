25afaf63d4178323477a3ba74060846030a34ff9
yoko-Developer <whitesnow.wave@gmail.com>
2024-09-15 21:12:18 +0900

    DIを使用してリファクタリング

--- src/main/java/com/example/todo_api/service/sample/SampleService.java
+++ src/main/java/com/example/todo_api/service/sample/SampleService.java
@@ -1,5 +1,8 @@
 package com.example.todo_api.service.sample;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SampleService {
     public SamleEntity find() {
         return new SamleEntity("Hello");
