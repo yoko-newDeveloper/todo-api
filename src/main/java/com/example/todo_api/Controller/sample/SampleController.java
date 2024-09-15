3f1657e4b0f3ad7e3064f04e00bd1ae3e1e0ea5b
yoko-Developer <whitesnow.wave@gmail.com>
2024-09-15 20:33:58 +0900

    ServiceとEntityの実装

--- src/main/java/com/example/todo_api/Controller/sample/SampleController.java
+++ src/main/java/com/example/todo_api/Controller/sample/SampleController.java
@@ -1,6 +1,7 @@
 package com.example.todo_api.Controller.sample;
 
 
+import com.example.todo_api.service.sample.SampleService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +12,11 @@ import java.time.LocalDateTime;
 @RequestMapping("/samples")
 public class SampleController {
 
+    private final SampleService service = new SampleService();
+
     @GetMapping
     public SampleDTO index() {
-        return new SampleDTO("OK", LocalDateTime.now());
+        var entity = service.find();
+        return new SampleDTO(entity.getContent(), LocalDateTime.now());
     }
 }
---
+++ src/main/java/com/example/todo_api/service/sample/SamleEntity.java
@@ -0,0 +1,8 @@
+package com.example.todo_api.service.sample;
+
+import lombok.Value;
+
+@Value
+public class SamleEntity {
+    String content;
+}
