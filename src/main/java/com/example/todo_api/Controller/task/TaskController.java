0984472384173467b0faffe7429e37e43d8f986d
yochan0120 <yoko_whiterosa.alba@gamma.ocn.ne.jp>
2024-09-28 22:24:04 +0900

    controller作成

--- src/main/java/com/example/todo_api/Controller/task/TaskController.java
+++ src/main/java/com/example/todo_api/Controller/task/TaskController.java
@@ -1,15 +1,18 @@
 package com.example.todo_api.Controller.task;
 
 import com.example.todoapi.controller.TasksApi;
+import com.example.todoapi.model.TaskDTO;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.config.Task;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TaskController implements TasksApi {
 
     @Override
-    public ResponseEntity<Void> showTask() {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<TaskDTO> showTask() {
+        var body = new TaskDTO();
+        body.setId(1L);
+        body.setTitle("TEST");
+        return ResponseEntity.ok(body);
     }
 }
